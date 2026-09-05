84a6c39bbe1667ec0a2c43ed93736b233e33b5ae
SkyHorizon3 <neuner.r@gmx.de>
2026-09-05 13:43:01 +0200

    fix: template again
    # Conflicts:
    #	include/RE/C/CombatBehaviorTreeNode.h

--- include/RE/C/CombatBehaviorTreeNode.h
+++ include/RE/C/CombatBehaviorTreeNode.h
@@ -2,12 +2,12 @@
 
 #include "RE/B/BSFixedString.h"
 #include "RE/B/BSTArray.h"
+#include "RE/C/CombatBehaviorThread.h"
 
 namespace RE
 {
 	class BGSLoadFormBuffer;
 	class BGSSaveFormBuffer;
-	class CombatBehaviorThread;
 
 	class CombatBehaviorTreeNode
 	{
@@ -18,6 +18,7 @@ namespace RE
 		CombatBehaviorTreeNode();
 		virtual ~CombatBehaviorTreeNode() = default;  // 00
 
+<<<<<<< HEAD
 		virtual const BSFixedString& GetName() { return name; };                                                                                         // 01
 		virtual void                 Enter([[maybe_unused]] CombatBehaviorThread* a_thread){};                                                           // 02
 		virtual void                 Exit([[maybe_unused]] CombatBehaviorThread* a_thread){};                                                            // 03
@@ -27,6 +28,17 @@ namespace RE
 		virtual void                 LoadGame([[maybe_unused]] CombatBehaviorThread* a_thread, [[maybe_unused]] BGSLoadFormBuffer* a_loadGameBuffer){};  // 07
 		virtual bool                 Validate([[maybe_unused]] const CombatBehaviorTreeNode* a_node) { return true; };                                   // 08
 		virtual const BSFixedString& GetType() = 0;
+=======
+		virtual const BSFixedString& GetName() { return name; };                                                                                          // 01
+		virtual void                 Enter([[maybe_unused]] CombatBehaviorThread* a_thread) {};                                                           // 02
+		virtual void                 Exit([[maybe_unused]] CombatBehaviorThread* a_thread) {};                                                            // 03
+		virtual void                 Update(CombatBehaviorThread* a_thread) { a_thread->Ascend(); };                                                      // 04
+		virtual void                 Abort(CombatBehaviorThread* a_thread) { a_thread->Ascend(); };                                                       // 05
+		virtual void                 SaveGame([[maybe_unused]] CombatBehaviorThread* a_thread, [[maybe_unused]] BGSSaveFormBuffer* a_saveGameBuffer) {};  // 06
+		virtual void                 LoadGame([[maybe_unused]] CombatBehaviorThread* a_thread, [[maybe_unused]] BGSLoadFormBuffer* a_loadGameBuffer) {};  // 07
+		virtual bool                 Validate([[maybe_unused]] const CombatBehaviorTreeNode* a_node) { return true; };                                    // 08
+		virtual const BSFixedString& GetType() = 0;                                                                                                       // 09
+>>>>>>> d4cf4534a (fix: template again)
 
 		void AddChild(CombatBehaviorTreeNode* a_child);
 		void AddChildren(const BSTArray<CombatBehaviorTreeNode*>& a_children);
